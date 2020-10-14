c016a5e2f7c504b2c4d818656c67f597a489190c
Artur Szerejko <szerejko.artur@gmail.com>
2020-10-14 17:12:30 -0400

    working check_path and king_viable_moves

--- lib/main.rb
+++ lib/main.rb
@@ -11,19 +11,25 @@ require_relative 'king'
 require_relative 'bishop'
 
 game = Board.new
-piece_two = Knight.new('black', ['G', '6'])
+@white_pieces_in_play = []
+@black_pieces_in_play = []
+
+piece_two = Knight.new('black', ['D', '4'])
 piece_one = Pawn.new('black', ['B', '7'])
 piece_three = Knight.new('white', ['C','6'])
-piece_four = King.new('black', ['D', '7'])
+piece_four = King.new('black', ['C', '7'])
 piece_five = Rook.new('white', ['E', '3'])
 piece_six = Bishop.new('white', ['F', '4'])
 piece_seven = Queen.new('white', ['E', '4'])
 
+[piece_two, piece_one, piece_four].each { |piece| @black_pieces_in_play << piece }
+[piece_three, piece_five, piece_six, piece_seven].each { |piece| @white_pieces_in_play << piece }
+
 
 #placing piece into new square
-g6 = game.retrieve_square("G6")
-g6.occupied_by = piece_two
-piece_two.current_square = g6.co_ord
+d4 = game.retrieve_square("D4")
+d4.occupied_by = piece_two
+piece_two.current_square = d4.co_ord
 
 b7 = game.retrieve_square("B7")
 b7.occupied_by = piece_one
@@ -33,9 +39,9 @@ c6 = game.retrieve_square('C6')
 c6.occupied_by = piece_three
 piece_three.current_square = c6.co_ord
 
-d7 = game.retrieve_square('D7')
-d7.occupied_by = piece_four
-piece_four.current_square = d7.co_ord
+c7 = game.retrieve_square('C7')
+c7.occupied_by = piece_four
+piece_four.current_square = c7.co_ord
 
 e3 = game.retrieve_square('E3')
 e3.occupied_by = piece_five
@@ -82,8 +88,8 @@ def check_path(piece, game)
     taken_column_ascii = square.first.ord
     taken_row_num = square.last.to_i
     
-    steps = (1..7)
     up_left_diagonal, up_right_diagonal, down_left_diagonal, down_right_diagonal = [], [], [], []
+    steps = (1..7)
     steps.each do |step| 
       up_left_diagonal << [(piece_column_ascii - step).chr, (piece_row_num + step).to_s]
       up_right_diagonal << [(piece_column_ascii + step).chr, (piece_row_num + step).to_s]
@@ -91,66 +97,119 @@ def check_path(piece, game)
       down_right_diagonal << [(piece_column_ascii - step).chr, (piece_row_num + step).to_s]
     end 
 
-    # duplicate for opp color with to_omit taking > instead of >=
-    if game.retrieve_square(square).occupied_by.color == piece.color
+    to_omit = []
+
+    if game.retrieve_square(square).occupied_by.color == piece.color #same color
       if up_left_diagonal.include?(square)
-        to_omit = piece.moves.select do |move|
-          up_left_diagonal.include?(move) && move.last.to_i >= taken_row_num
+        piece.moves.each do |move|
+          to_omit << move if up_left_diagonal.include?(move) && move.last.to_i >= taken_row_num
         end
-        piece.moves.delete_if { |move| to_omit.include?(move) }
       elsif up_right_diagonal.include?(square)
-        to_omit = piece.moves.select do |move|
-          up_right_diagonal.include?(move) && move.last.to_i >= taken_row_num
+        piece.moves.each do |move|
+          to_omit << move if up_right_diagonal.include?(move) && move.last.to_i >= taken_row_num
         end
-        piece.moves.delete_if { |move| to_omit.include?(move) }
       elsif down_left_diagonal.include?(square)
-        to_omit = piece.moves.select do |move|
-          down_left_diagonal.include?(move) && move.last.to_i <= taken_row_num
+        piece.moves.each do |move|
+          to_omit << move if down_left_diagonal.include?(move) && move.last.to_i <= taken_row_num
         end
-        piece.moves.delete_if { |move| to_omit.include?(move) }
       elsif down_right_diagonal.include?(square)
-        to_omit = piece.moves.select do |move|
-          down_right_diagonal.include?(move) && move.last.to_i <= taken_row_num
+        piece.moves.each do |move|
+          to_omit << move if down_right_diagonal.include?(move) && move.last.to_i <= taken_row_num
         end
-        piece.moves.delete_if { |move| to_omit.include?(move) }
       elsif (taken_column_ascii < piece_column_ascii) && (taken_row_num == piece_row_num) #left
-        to_omit = piece.moves.select do |move|
-          (move.first.ord <= taken_column_ascii) && (move.last.to_i == taken_row_num)
+        piece.moves.each do |move|
+          to_omit << move if (move.first.ord <= taken_column_ascii) && (move.last.to_i == taken_row_num)
         end
-        piece.moves.delete_if { |move| to_omit.include?(move) }
       elsif (taken_column_ascii > piece_column_ascii) && (taken_row_num == piece_row_num) #right
-        to_omit = piece.moves.select do |move|
-          (move.first.ord >= taken_column_ascii) && (move.last.to_i == taken_row_num)
+        piece.moves.each do |move|
+          to_omit << move if (move.first.ord >= taken_column_ascii) && (move.last.to_i == taken_row_num)
         end
-        piece.moves.delete_if { |move| to_omit.include?(move) }
       elsif (taken_column_ascii == piece_column_ascii) && (taken_row_num > piece_row_num) #above
-        to_omit = piece.moves.select do |move|
-          (move.first.ord == taken_column_ascii) && (move.last.to_i >= taken_row_num)
+        piece.moves.each do |move|
+          to_omit << move if (move.first.ord == taken_column_ascii) && (move.last.to_i >= taken_row_num)
         end
-        piece.moves.delete_if { |move| to_omit.include?(move) }
       elsif (taken_column_ascii == piece_column_ascii) && (taken_row_num < piece_row_num) #below
-        to_omit = piece.moves.select do |move|
-          (move.first.ord == taken_column_ascii) && (move.last.to_i <= taken_row_num)
+        piece.moves.each do |move|
+          to_omit << move if (move.first.ord == taken_column_ascii) && (move.last.to_i <= taken_row_num)
         end
-        piece.moves.delete_if { |move| to_omit.include?(move) }
       end
     end
+
+    if game.retrieve_square(square).occupied_by.color != piece.color #opp color
+      if up_left_diagonal.include?(square)
+        piece.moves.each do |move|
+          to_omit << move if up_left_diagonal.include?(move) && move.last.to_i > taken_row_num
+        end
+      elsif up_right_diagonal.include?(square)
+        piece.moves.each do |move|
+          to_omit << move if up_right_diagonal.include?(move) && move.last.to_i > taken_row_num
+        end
+      elsif down_left_diagonal.include?(square)
+        piece.moves.each do |move|
+          to_omit << move if down_left_diagonal.include?(move) && move.last.to_i < taken_row_num
+        end
+      elsif down_right_diagonal.include?(square)
+        piece.moves.each do |move|
+          to_omit << move if down_right_diagonal.include?(move) && move.last.to_i < taken_row_num
+        end
+      elsif (taken_column_ascii < piece_column_ascii) && (taken_row_num == piece_row_num) #left
+        piece.moves.each do |move|
+          to_omit << move if (move.first.ord < taken_column_ascii) && (move.last.to_i == taken_row_num)
+        end
+      elsif (taken_column_ascii > piece_column_ascii) && (taken_row_num == piece_row_num) #right
+        piece.moves.each do |move|
+          to_omit << move if (move.first.ord > taken_column_ascii) && (move.last.to_i == taken_row_num)
+        end
+      elsif (taken_column_ascii == piece_column_ascii) && (taken_row_num > piece_row_num) #above
+        piece.moves.each do |move|
+          to_omit << move if (move.first.ord == taken_column_ascii) && (move.last.to_i > taken_row_num)
+        end
+      elsif (taken_column_ascii == piece_column_ascii) && (taken_row_num < piece_row_num) #below
+        piece.moves.each do |move|
+          to_omit << move if (move.first.ord == taken_column_ascii) && (move.last.to_i < taken_row_num)
+        end
+      end
+    end
+
+    piece.moves.delete_if { |move| to_omit.include?(move) }
   end
-
 end
 
-def king_viable_moves #will check if any moves within king's move_set put him into check
+def king_viable_moves(piece)
+  # runs create_moves for all opp pieces in play...
+  # removes any moves from king's move_set that overlap with opp pieces' move_sets
+  opp_pieces, opp_moves = [], []
+  piece.color == 'black' ? @white_pieces_in_play.each { |piece| opp_pieces << piece } : @black_pieces_in_play.each { |piece| opp_pieces << piece }
+  opp_pieces.each do |piece| 
+    piece.create_moves 
+    piece.moves.each { |move| opp_moves << move }
+  end
+  piece.moves.delete_if { |move| opp_moves.include?(move) }
 end
+
+
+# game will have white_pieces_in_play and black_pieces_in_play instance variables 
+# that will be used for king_viable_moves etc....
+
+
 
 #create move options, check path to remove paths w/obstructions and then show possible path
-
 #specific order of ops for pawn
 # piece_one.create_moves
 # pawn_diag(piece_one, game)
 # game.show_path(b7)
 
-piece_seven.create_moves
-check_path(piece_seven, game)
-game.show_path(e4)
+# piece_seven.create_moves
+# check_path(piece_seven, game)
+# game.show_path(e4)
+
+# piece_five.create_moves
+# check_path(piece_five, game)
+# game.show_path(e3)
+
+piece_four.create_moves
+check_path(piece_four, game)
+king_viable_moves(piece_four)
+game.show_path(c7)
 
 game.draw
